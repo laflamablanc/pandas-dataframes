7d9aefe020b39494aa2da249b067d743d05e3128
Sean LaFlam <sean.laflam@gmail.com>
2021-06-28 23:44:24 -0400

    Outfile

--- part1.py
+++ part1.py
@@ -1,3 +1,17 @@
 import csv
 
-with open("Grades.csv", r) as infile:
+outfile = open("student_name.csv", 'w')
+
+with open("Grades.csv", 'r') as infile:
+    reader = csv.reader(infile, delimiter=",")
+    header = next(reader)
+    
+    for row in reader:
+        student_first_name = row[0]
+        student_last_name = row[1]
+        student_year = row[2]
+        student_grade = row[3]
+        # print(student_first_name, student_last_name, student_year, student_grade )
+        line = "{},{}\n".format(student_first_name, student_last_name)
+        outfile.write(line)
+outfile.close()
